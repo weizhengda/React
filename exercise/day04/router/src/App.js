7ff68ce7dd9e7fed29c9dcf36bf02293ed2521e6
weizhengda <2625785357@qq.com>
2017-06-03 12:08:21 +0800

    day04

--- exercise/day04/router/src/App.js
+++ exercise/day04/router/src/App.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import './components/static/css/index.css';
+
+import { Link } from 'react-router';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <div className="header">
+           <ul>
+             <li>
+               <Link to='/home'>首页</Link>
+             </li>
+             <li>
+               <Link to='/news'>新闻</Link>
+             </li>
+           </ul>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="content">
+              {this.props.children}
+        </div>
       </div>
     );
   }
---
+++ exercise/day04/router/src/components/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Home extends React.Component{
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return(
+            <div>
+                首页模板
+            </div>
+        )
+    }
+}
+export default Home;
